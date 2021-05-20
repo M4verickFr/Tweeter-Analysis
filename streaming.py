adf35d02b15055a6d58851b081e7778aab2bf23a
caullird <caullireau.dorian@gmail.com>
2021-05-20 09:42:00 +0200

    restructur insert on db

--- streaming.py
+++ streaming.py
@@ -68,26 +68,3 @@ class StdOutListener(StreamListener):
     def on_error(self, status):
         return False
 
-if __name__ == '__main__':
-    sql_insert_city = "INSERT INTO `city`(`place_id`, `display_name`, `polygon`, `lat`, `lon`) VALUES (%s,%s,%s,%s,%s)"
-    sql_insert_tweet = "INSERT INTO `tweet`(`id_tweet`, `created_at`, `full_text`, `lang`, `retweet_count`, `latitude`, `longitude`, `id_city`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"      
-    sql_insert_word = "INSERT INTO `word`(`word`, `id_city`) VALUES (%s,%s)"
-    
-    bdd = database("localhost","root","","twitter")
-    bdd.connect()
-    
-    listener = StdOutListener()
-    auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-    api = API(auth, wait_on_rate_limit=True)
-
-    location = [-5.225, 41.333, 9.55, 51.2]
-    track = ['France','Polytech','Ocean','Ville','Ecole','Ingenieur','Annecy','Voyage','openstreetmap'
-             'Grenoble','Paris','Lyon','ia','adwords','RGPD','CNIL','Cookie','Instagram','GoogleMaps']
-    languages = ['fr']
-    dictonnary = enchant.Dict("fr_FR")
-    
-    toknizer = RegexpTokenizer(r'''\w'|\w+|[^\w\s]''')
-
-    stream = Stream(auth, listener, tweet_mode='extended')
-    stream.filter(locations=location,languages=languages,track=track)
